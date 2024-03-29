fb2ae361e73891c3577ac54dff3633cf465d48ae
Vansh Shewaramani <Vansh-1021@users.noreply.github.com>
2024-03-29 12:58:29 +0530

    A courier website

--- ParcelPal/JS/home.js
+++
@@ -1,7 +0,0 @@
-const toggleMenu = () => {
-	const menu = document.querySelector('nav ul');
-	menu.classList.toggle('show');
-};
-
-const navLinks = document.querySelectorAll('nav ul li');
-navLinks.forEach(link => link.addEventListener('click', toggleMenu));
